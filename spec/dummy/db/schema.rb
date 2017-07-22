ea69b671d91b74591f6f7e10e0eb00eecd74a020
Sebastian Skałacki <skalee@gmail.com>
2017-07-22 18:19:47 +0200

    Implement a first meaningful test
    Implement the first spec which actually ensures that gem is doing what
    is expected.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,3 +1,8 @@
 ActiveRecord::Schema.define do
-  #
+  create_table(:users, force: true) do |t|
+    t.string :first_name
+    t.string :last_name
+    t.string :email
+    t.timestamps
+  end
 end
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -4,8 +4,36 @@
 require "spec_helper"
 
 RSpec.describe "Attr Masker gem" do
-  example "The Rake task doesn't raise any exceptions" do
-    expect { run_rake_task }.not_to raise_exception
+  before do
+    stub_const "User", Class.new(ActiveRecord::Base)
+  end
+
+  let!(:han) do
+    User.create!(
+      first_name: "Han",
+      last_name: "Solo",
+      email: "han@example.test",
+    )
+  end
+
+  let!(:luke) do
+    User.create!(
+      first_name: "Luke",
+      last_name: "Skywalker",
+      email: "luke@jedi.example.test",
+    )
+  end
+
+  example "Masking a single text attribute with default options" do
+    User.class_eval do
+      attr_masker :last_name
+    end
+
+    expect { run_rake_task }.not_to(change { User.count })
+
+    expect { han.reload }.to(change { han.last_name }.to("(redacted)"))
+
+    expect(1).to eq(1)
   end
 
   def run_rake_task
