7acc7d702382d4fe50291f25187a5f7caaa550e4
Sebastian Skałacki <skalee@gmail.com>
2017-08-02 19:58:38 +0200

    Define Attribute#evaluate_option
    Move logic from attr_masker.rb where it should belong to.  Add tests.

--- spec/attribute_spec.rb
+++ spec/attribute_spec.rb
@@ -30,4 +30,37 @@ RSpec.describe AttrMasker::Attribute do
       expect(subject.call).to eq(:some_column)
     end
   end
+
+  describe "#evaluate_option" do
+    subject { receiver.method :evaluate_option }
+    let(:receiver) { described_class.new :some_attr, model_instance, options }
+    let(:options) { {} }
+    let(:model_instance) { double }
+    let(:retval) { subject.call(:option_name, model_instance) }
+
+    context "when that option value is a symbol" do
+      let(:options) { { option_name: :meth } }
+
+      before do
+        allow(model_instance).to receive(:meth).with(no_args).and_return(:rv)
+      end
+
+      it "evaluates an object's method pointed by that symbol" do
+        expect(retval).to be(:rv)
+      end
+    end
+
+    context "when that option_nameion value responds to #call" do
+      let(:options) { { option_name: callable } }
+      let(:callable) { double }
+
+      before do
+        allow(callable).to receive(:call).with(model_instance).and_return(:rv)
+      end
+
+      it "calls #call on it passing model instance as the only argument" do
+        expect(retval).to be(:rv)
+      end
+    end
+  end
 end
