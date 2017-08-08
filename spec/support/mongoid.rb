fac6778d44f9b2511721dc48e73fd62624de4fa3
Sebastian Skałacki <skalee@gmail.com>
2017-08-08 16:59:44 +0200

    Configure Mongoid client for tests

---
+++ spec/support/mongoid.rb
@@ -0,0 +1,7 @@
+# Copied from Mongoid's test suite
+# https://github.com/mongodb/mongoid/blob/v6.2.0/spec/spec_helper.rb
+
+# These environment variables can be set if wanting to test against a database
+# that is not on the local machine.
+ENV["MONGOID_SPEC_HOST"] ||= "127.0.0.1"
+ENV["MONGOID_SPEC_PORT"] ||= "27017"
