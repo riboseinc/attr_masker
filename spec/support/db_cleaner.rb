05b6dbc37b51f495424d6002a5a1315a5b93a92b
Sebastian Skałacki <skalee@gmail.com>
2017-08-11 04:22:31 +0200

    Reconfigure Database Cleaner for Mongoid
    Set up Database Cleaner to handle Mongoid as well.

--- spec/support/db_cleaner.rb
+++ spec/support/db_cleaner.rb
@@ -2,9 +2,13 @@ require "database_cleaner"
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner[:active_record].strategy = :truncation
+    # Since models are defined dynamically in specs, Database Cleaner is unable
+    # to list them and to determine collection names to be cleaned.
+    # Therefore, they are specified explicitly here.
+    DatabaseCleaner[:mongoid].strategy = :truncation, { only: "users" }
 
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.around(:each) do |example|
