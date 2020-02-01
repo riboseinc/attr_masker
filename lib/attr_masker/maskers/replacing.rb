dcb7dd017cbd442bafbd6ecf3616fad5b37d2eff
Sebastian Skałacki <skalee@gmail.com>
2020-02-01 17:03:29 +0100

    Write RDocs for built-in maskers

--- lib/attr_masker/maskers/replacing.rb
+++ lib/attr_masker/maskers/replacing.rb
@@ -2,14 +2,30 @@
 #
 module AttrMasker
   module Maskers
-    # This default masker simply replaces any value with a fixed string.
+    # +Replacing+ masker replaces every character of string which is being
+    # masked with +replacement+ one, preserving the length of the masked string
+    # (provided that a replacement string contains a single character, which is
+    # a typical case). Optionally, non-alphanumeric characters like dashes or
+    # spaces may be left unchanged.
     #
-    # +opts+ is a Hash with the key :value that gives you the current attribute
-    # value.
+    # @example Would mask "Adam West" as "XXXXXXXXX"
+    #   class User < ActiveRecord::Base
+    #     m = AttrMasker::Maskers::Replacing.new(replacement: "X")
+    #     attr_masker :name, :masker => m
+    #   end
     #
+    # @example Would mask "123-456-789" as "XXX-XXX-XXX"
+    #   class User < ActiveRecord::Base
+    #     m = AttrMasker::Maskers::Replacing.new(
+    #         replacement: "X", alphanum_only: true)
+    #     attr_masker :phone, :masker => m
+    #   end
     class Replacing
       attr_reader :replacement, :alphanum_only
 
+      # @param replacement [String] replacement string
+      # @param alphanum_only [Boolean] whether to leave non-alphanumeric
+      #   characters unchanged or not
       def initialize(replacement: "*", alphanum_only: false)
         replacement = "" if replacement.nil?
         @replacement = replacement
